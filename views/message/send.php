cbd8864f51493f6bab35c206df065deb0446d4b1
sanduninduwara <sanduninduwara2@gmail.com>
2022-01-08 13:34:57 +0530

    msg css

--- views/message/send.php
+++ views/message/send.php
@@ -32,16 +32,29 @@
 
         <form class="m-3" action=<?= BASE_DIR . "Message/send" ?> method="post">
 
-            <label for="message_type">TO</label>
-            <select name="message_type">
-                <?php
-                    foreach ($_SESSION['data'] as $value => $text)
-                        echo "<option value=".$value.">".$text."</option>";
-                ?>
-            </select><br>
 
-            <input type="text" name="message">
-            <input type="submit" value="SEND">
+            <div class="d-flex justify-content-center flex-row bd-highlight mb-3">
+                <!-- <label for="message_type">TO</label> -->
+                <div class="d-flex flex-column m-1 bd-highlight ">
+
+                    <select class="form-select form-select-sm" name="message_type">
+                        <?php
+                        foreach ($_SESSION['data'] as $value => $text)
+                            echo "<option value=" . $value . ">" . $text . "</option>";
+                        ?>
+                    </select>
+                </div>
+
+            </div>
+
+
+            <div class="d-flex justify-content-center flex-row bd-highlight mb-3">
+
+                <input class="form-control" placeholder="type your msg here" type="text" name="message">
+                <input class='btn btn-outline-light m-2 ' type="submit" value="SEND">
+            </div>
+
+
 
 
 
