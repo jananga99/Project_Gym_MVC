8c96b1375827c768ed238f1195dd338930ad4255
sanduninduwara <sanduninduwara2@gmail.com>
2022-01-20 17:10:46 +0530

    report admin panal css

--- views/Admin/view/Report.php
+++ views/Admin/view/Report.php
@@ -5,42 +5,69 @@ $reports = $_SESSION['reports'];
 ?>
 
 <html>
-    <head>
-        <title>Reports</title>
-    </head>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    require_once 'public/html/boostraplinks.html';
+    ?>
+    <link rel="stylesheet" href=<?= BASE_DIR . "public/css/report.css" ?>>
+
+    <title>Reports</title>
+</head>
 
 <body>
 
-<table>
-<thead>
-    <tr>
-        <th>Reason</th>
-        <th>Email</th>
-    </tr>
-</thead>
-<tbody>
-    <?php foreach($reports as $report): ?>
-        
-        <tr>
-            <td><?php echo $report['Reason'] ?> </td>
-            <td><?php echo $report['Email'] ?> </td>
+    <?php
+    $menu_arr = array(
+        "Dashboard" => BASE_DIR . $_SESSION['logged_user']['type'],
 
-            
-                <td>
-                <a href=<?= BASE_DIR . "Admin/ignore_report/".$report['Email']?>>
-                <button>Ignore</button>
-                </a>
-                </td>
-            
+        "Messages" => BASE_DIR . "Message",
+        "Log Out" => BASE_DIR . "Auth/logout"
+    );
+    $navbar =  new Navbar($menu_arr);
+    echo $navbar->get();
+    ?>
 
-            <td><button>Ban</button>
-        </tr>
-        
-        <?php endforeach?>
-</tbody>
-</table>
 
+    <div class="container">
+        <table class='table table-hover' style='color:white'>
+            <thead>
+                <tr>
+                    <th>Reason</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($reports as $report) : ?>
+
+                    <tr>
+                        <td><?php echo $report['Reason'] ?> </td>
+                        <td><?php echo $report['Email'] ?> </td>
+
+
+                        <td>
+                            <a href=<?= BASE_DIR . "Admin/ignore_report/" . $report['Email'] ?>>
+                                <button class="btn btn-primary">Ignore</button>
+                            </a>
+                        </td>
+
+
+                        <td><button class="btn btn-danger">Ban</button></td>
+                    </tr>
+
+                <?php endforeach ?>
+            </tbody>
+        </table>
+
+    </div>
+
+
+    <?php
+    require_once 'public/html/footer.html';
+    ?>
 </body>
 
 </html>
-
